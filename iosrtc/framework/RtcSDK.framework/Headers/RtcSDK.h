e14a2fd191dfc5e5a55830fc36eb8ddf62285a6a
zcweicheng <weichengzheng@fancyjing.com>
2024-03-18 16:27:25 +0800

    更新IOS RTCSDK framework

--- iosrtc/framework/RtcSDK.framework/Headers/RtcSDK.h
+++ iosrtc/framework/RtcSDK.framework/Headers/RtcSDK.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double RtcSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char RtcSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RtcSDK/PublicHeader.h>
-
-
